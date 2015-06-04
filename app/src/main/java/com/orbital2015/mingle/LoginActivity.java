94938d039a7007c5180325a268c03c7d0d849c9e
HooDeLin <hoodelin0619@gmail.com>
2015-06-04 11:12:07 +0800

    Create Login activity

---
+++ app/src/main/java/com/orbital2015/mingle/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.orbital2015.mingle;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private EditText loginUsernameEditText;
+    private EditText loginPasswordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginUsernameEditText = (EditText) findViewById(R.id.loginUsernameEditText);
+        loginPasswordEditText = (EditText) findViewById(R.id.loginPasswordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkCredentials(loginUsernameEditText.getText().toString(), loginPasswordEditText.getText().toString())){
+                    //TODO: start next activity and start sinch service
+                    Toast.makeText(getApplicationContext(), "Login Success",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect Username/Password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean checkCredentials(String userName, String password){
+        //TODO: authentication logic here
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
